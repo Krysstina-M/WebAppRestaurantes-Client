b8cefa756434aa0efb209cd6a0fda4bff740e4ae
Cristina <kristina.s.milea@gmail.com>
2022-05-24 17:18:58 +0200

    Quitados TODOs que no se pueden hacer

--- src/App.vue
+++ src/App.vue
@@ -33,8 +33,6 @@
 //TODO Probar a meter imagen desde Desktop con BLOB
 //FIXME No funciona en git
 //FIXME A veces no refresca la web con los nuevos cambios
-//TODO juntar en un component lo de alta y modificacion
-//TODO juntar en un component lo de ver restaurante, random y destacado
 import Vue from "vue";
 
 Vue.component("errorNomDir", {
