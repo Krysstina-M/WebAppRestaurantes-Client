e72cf3e02a358779ed3173dc1f928fbbd26fbed6
Cristina <kristina.s.milea@gmail.com>
2022-06-08 09:31:41 +0200

    Hecho el navbar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,86 @@
 <template>
     <div id="app">
+        <nav class="navbar navbar-expand-lg fixed-top py-0">
+            <div class="container-fluid">
+                <a
+                    class="navbar-brand"
+                    href="https://vuejs.org/"
+                    target="_blank"
+                >
+                    <img
+                        class="d-inline-block align-text-center me-2"
+                        src="./assets/logo-blanco.png"
+                        alt=""
+                        width="30"
+                        height="25"
+                    />{{ titulo }}
+                </a>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    class="navbar-collapse collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/">Home</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/get-restaurantes"
+                                >Restaurantes</router-link
+                            >
+                        </li>
+                        <li class="nav-item">
+                            <router-link
+                                class="nav-link"
+                                :to="{
+                                    name: 'restaurante-destacado',
+                                    params: { id: 0 },
+                                }"
+                                >Restaurante destacado</router-link
+                            >
+                        </li>
+                        <li class="nav-item">
+                            <router-link
+                                class="nav-link"
+                                :to="{
+                                    name: 'restaurante-random',
+                                    params: { id: 0 },
+                                }"
+                                ><a>¿Dónde comer?</a></router-link
+                            >
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/contacto"
+                                >Contacto</router-link
+                            >
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav navbar-right">
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/contacto"
+                                >Registrarse</router-link
+                            >
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link" to="/contacto"
+                                >Iniciar sesión</router-link
+                            >
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <img src="../dist/logo.png" />
-        <h1>{{ msg }}</h1>
-        <!--Menú de arriba-->
-        <ul class="top">
-            <li><router-link to="/">Home</router-link></li>
-            <li>
-                <router-link to="/get-restaurantes">Restaurantes</router-link>
-            </li>
-            <li>
-                <router-link
-                    :to="{ name: 'restaurante-destacado', params: { id: 0 } }"
-                    >Restaurante destacado</router-link
-                >
-            </li>
-            <li>
-                <router-link
-                    :to="{ name: 'restaurante-random', params: { id: 0 } }"
-                    >¿Dónde comer?</router-link
-                >
-            </li>
-            <li><router-link to="/contacto">Contacto</router-link></li>
-        </ul>
 
         <!--Router-->
         <router-view></router-view>
@@ -30,6 +89,8 @@
 
 <script>
 //TODO meter bootstrap?
+//TODO lo de active en el navbar
+//TODO mirar lo del fa fa font
 //TODO probar filepond para meter la ruta de la imagen en la bd
 //FIXME No funciona en git
 import Vue from "vue";
@@ -74,14 +135,110 @@ export default {
     name: "app",
     data() {
         return {
-            msg: "Restaurantes",
+            titulo: "Restaurantes",
         };
+    },
+    mounted() {
+        /*var header = document.getElementById("app");
+        var btns = header.getElementsByClassName("nav-item");
+
+        for (var i = 0; i < btns.length; i++) {
+            btns[i].addEventListener("click", function () {
+                var current = document.getElementsByClassName("active");
+                current[0].className = current[0].className.replace(
+                    " active",
+                    ""
+                );
+                this.className += " active";
+            });
+        }*/
     },
 };
 </script>
 
 <style>
-/*App*/
+:root {
+    --bs-blue: #0d6efd;
+    --bs-indigo: #6610f2;
+    --bs-purple: #6f42c1;
+    --bs-pink: #d63384;
+    --bs-red: #dc3545;
+    --bs-orange: #fd7e14;
+    --bs-yellow: #ffc107;
+    --bs-green: #198754;
+    --bs-teal: #20c997;
+    --bs-cyan: #0dcaf0;
+    --bs-black: #000;
+    --bs-white: #fff;
+    --bs-gray: #6c757d;
+    --bs-gray-dark: #343a40;
+    --bs-gray-100: #f8f9fa;
+    --bs-gray-200: #e9ecef;
+    --bs-gray-300: #dee2e6;
+    --bs-gray-400: #ced4da;
+    --bs-gray-500: #adb5bd;
+    --bs-gray-600: #6c757d;
+    --bs-gray-700: #495057;
+    --bs-gray-800: #343a40;
+    --bs-gray-900: #212529;
+    --bs-primary: #0d6efd;
+    --bs-secondary: #6c757d;
+    --bs-success: #198754;
+    --bs-info: #0dcaf0;
+    --bs-warning: #ffc107;
+    --bs-danger: #dc3545;
+    --bs-light: #f8f9fa;
+    --bs-dark: #212529;
+    --bs-primary-rgb: 13, 110, 253;
+    --bs-secondary-rgb: 108, 117, 125;
+    --bs-success-rgb: 25, 135, 84;
+    --bs-info-rgb: 13, 202, 240;
+    --bs-warning-rgb: 255, 193, 7;
+    --bs-danger-rgb: 220, 53, 69;
+    --bs-light-rgb: 248, 249, 250;
+    --bs-dark-rgb: 33, 37, 41;
+    --bs-white-rgb: 255, 255, 255;
+    --bs-black-rgb: 0, 0, 0;
+    --bs-body-color-rgb: 33, 37, 41;
+    --bs-body-bg-rgb: 255, 255, 255;
+    --bs-font-sans-serif: system-ui, -apple-system, "Segoe UI", Roboto,
+        "Helvetica Neue", "Noto Sans", "Liberation Sans", Arial, sans-serif,
+        "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+        "Noto Color Emoji";
+    --bs-font-monospace: SFMono-Regular, Menlo, Monaco, Consolas,
+        "Liberation Mono", "Courier New", monospace;
+    --bs-gradient: linear-gradient(
+        180deg,
+        rgba(255, 255, 255, 0.15),
+        rgba(255, 255, 255, 0)
+    );
+    --bs-body-font-family: var(--bs-font-sans-serif);
+    --bs-body-font-size: 1rem;
+    --bs-body-font-weight: 400;
+    --bs-body-line-height: 1.5;
+    --bs-body-color: #212529;
+    --bs-body-bg: #fff;
+    --bs-border-width: 1px;
+    --bs-border-style: solid;
+    --bs-border-color: #dee2e6;
+    --bs-border-color-translucent: rgba(0, 0, 0, 0.175);
+    --bs-border-radius: 0.375rem;
+    --bs-border-radius-sm: 0.25rem;
+    --bs-border-radius-lg: 0.5rem;
+    --bs-border-radius-xl: 1rem;
+    --bs-border-radius-2xl: 2rem;
+    --bs-border-radius-pill: 50rem;
+    --bs-heading-color: ;
+    --bs-link-color: #0d6efd;
+    --bs-link-hover-color: #0a58ca;
+    --bs-code-color: #d63384;
+    --bs-highlight-bg: #fff3cd;
+    /*-----------------------------------------------------*/
+    --main-verde: #42b983;
+    --main-gris: #2c3e50;
+    --main-border: 1px solid black;
+}
+
 #app {
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     text-align: center;
@@ -89,31 +246,19 @@ export default {
     margin-top: 50px;
 }
 
-/*Barra menú*/
-.top {
-    list-style-type: none;
-    margin: auto;
-    overflow: hidden;
-    background-color: #42b983;
-    border: 1px solid #2c3e50;
+.navbar {
+    background-color: var(--main-verde);
+    border: var(--main-border);
+    color: var(--main-gris);
 }
 
-.top a {
-    display: block;
-    color: #2c3e50;
-    text-align: center;
-    padding: 10px;
-    text-decoration: none;
+.nav-item > a:hover,
+.active a {
+    background-color: var(--main-gris);
+    color: var(--bs-white);
 }
 
-.top a:hover {
-    background-color: #2c3e50;
-    color: white;
-}
-
-.top li {
-    display: inline-block;
-}
+/*---------------------------------------------------------------------------*/
 
 /*AltaRestaurante + ModificarRestaurante*/
 /*Flecha izquierda*/
